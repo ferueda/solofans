e0640c722e81799bde98cb2d1c1e29d41f37dfb8
ferueda <fe.rueda@outlook.com>
2020-07-12 23:07:41 -0400

    improve Login.js added and modified various styles and added loading and error states to the form in the Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Heading, Box, Stack, Link, Input, Button, Text } from '@chakra-ui/core';
+import { GrFacebookOption } from 'react-icons/gr';
 
 const dividerContainer = {
 	display: 'flex',
@@ -13,9 +14,12 @@ const dividerStyles = {
 	backgroundColor: '#CBD5E0',
 	width: '100%',
 };
+
 const Login = () => {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	const handleChange = (event, type) => {
 		if (type === 'username') {
@@ -27,39 +31,54 @@ const Login = () => {
 		}
 	};
 
+	const isInvalid = username === '' || password === '' || password.length < 4;
+
 	return (
 		<Box d="flex" alignItems="center" flexDirection="column" w="100%" py={10} px={10}>
 			<Heading size="2xl" textAlign="center" mb="2rem">
-				Desbloqueame
+				Solo<span style={{ color: '#3182CE' }}>Fans</span>
 			</Heading>
+
 			<Stack
 				d="flex"
 				bg="gray.50"
 				border="1px"
-				borderColor="gray.100"
+				borderColor="gray.300"
 				minW="400px"
 				rounded="md"
 				p="2rem"
+				pb="1rem"
 				spacing={3}
 				maxWidth="30rem"
 				mb="1rem"
 			>
 				<Input value={username} onChange={event => handleChange(event, 'username')} placeholder="Correo electrónico" />
+
 				<Input
 					value={password}
 					onChange={event => handleChange(event, 'password')}
 					type="password"
 					placeholder="Contraseña"
 				/>
+
 				<Button
-					isDisabled={username === '' || password === ''}
+					isDisabled={isInvalid}
+					isLoading={isLoading}
 					aria-label="ingresar"
 					variantColor="blue"
 					type="submit"
 					mt="1rem"
+					onClick={() => setIsLoading(true)}
 				>
 					Ingresar
 				</Button>
+
+				{error && (
+					<Text textAlign="center" color="red.500">
+						{error.message}
+					</Text>
+				)}
+
 				<div style={dividerContainer}>
 					<div style={{ ...dividerStyles, marginRight: '1rem' }}></div>
 					<Text fontWeight="500" color="gray.400">
@@ -67,12 +86,24 @@ const Login = () => {
 					</Text>
 					<div style={{ ...dividerStyles, marginLeft: '1rem' }}></div>
 				</div>
+
+				<Button aria-label="ingresar con facebook" color="#fff" bg="#3b5998" _hover={{ bg: '#1f4287' }} mt="1rem">
+					<span style={{ position: 'absolute', left: '0.5rem' }}>
+						<GrFacebookOption size={24} />
+					</span>
+					Ingresar con Facebook
+				</Button>
+
+				<Link to="#" textAlign="center" color="blue.500">
+					Recuperar clave
+				</Link>
 			</Stack>
+
 			<Stack
 				d="flex"
 				bg="gray.50"
 				border="1px"
-				borderColor="gray.100"
+				borderColor="gray.300"
 				minW="400px"
 				rounded="md"
 				p="1rem"
@@ -80,8 +111,8 @@ const Login = () => {
 				maxWidth="30rem"
 			>
 				<Text textAlign="center">
-					¿Aun no tienes una cuenta?{' '}
-					<Link color="blue.500">
+					¿Aún no tienes una cuenta?{' '}
+					<Link to="#" color="blue.500">
 						<strong>Regístrate</strong>
 					</Link>
 				</Text>
