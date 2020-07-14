cc925270ee26321a22252c1d430dc73ba1051994
ferueda <fe.rueda@outlook.com>
2020-07-14 16:07:33 -0400

    modify App structure added GlobalState, react router and the Home, login, SignUp and ForgotPassword routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
-import Login from './components/Login';
+
+import GlobalState from './GlobalState/GlobalState';
+import * as ROUTES from './constants/routes';
+
+import Home from './components/Home';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/SignUp';
+import ForgotPassword from './components/Auth/ForgotPassword';
 
 function App() {
 	return (
-		<ThemeProvider>
-			<CSSReset />
-			<Login />
-		</ThemeProvider>
+		<GlobalState>
+			<ThemeProvider>
+				<CSSReset />
+				<Router>
+					<Route exact path={ROUTES.HOME} component={Home} />
+
+					<Route exact path={ROUTES.LOGIN} component={Login} />
+					<Route exact path={ROUTES.SINGUP} component={SignUp} />
+					<Route exact path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
+				</Router>
+			</ThemeProvider>
+		</GlobalState>
 	);
 }
 
