8c459cca3de022515fea0e33695b61ede847f653
ferueda <fe.rueda@outlook.com>
2020-07-15 19:44:47 -0400

    add routes to routes.js added /buscar, /post/crear and /cuenta routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,7 @@
 export const HOME = '/';
+export const BUSCAR = '/buscar';
+export const NEW_POST = '/post/crear';
+export const ACCOUNT = '/cuenta';
 export const SINGUP = '/cuenta/registro';
 export const LOGIN = '/cuenta/ingreso';
 export const FORGOT_PASSWORD = '/cuenta/clave/recuperar';
