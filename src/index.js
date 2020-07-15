a9ac252ae5e939e42e66419c021b5f8edfeacd39
ferueda <fe.rueda@outlook.com>
2020-07-15 19:45:15 -0400

    add router to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routedApp = (
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
+
+ReactDOM.render(routedApp, document.getElementById('root'));
