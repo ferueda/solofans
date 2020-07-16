5a2e0051649deb7a3915ed035f2a3a7056663658
ferueda <fe.rueda@outlook.com>
2020-07-15 20:17:09 -0400

    update App.js added Home, Buscar, NewPost and Account components.
    added React Router and the routes for each component added.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, Switch, useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
 import GlobalState from './GlobalState/GlobalState';
 import * as ROUTES from './constants/routes';
 
-import Home from './components/Home';
+import Home from './components/Home/Home';
+import Buscar from './components/Buscar/Buscar';
+import NewPost from './components/Post/NewPost';
+import Account from './components/Account/Account';
+
 import Login from './components/Auth/Login';
 import SignUp from './components/Auth/SignUp';
 import ForgotPassword from './components/Auth/ForgotPassword';
 
-function App() {
+const App = () => {
+	const location = useLocation();
+
+	const background = location.state && location.state.background;
+
 	return (
 		<GlobalState>
 			<ThemeProvider>
 				<CSSReset />
-				<Router>
-					<Route exact path={ROUTES.HOME} component={Home} />
 
-					<Route exact path={ROUTES.LOGIN} component={Login} />
-					<Route exact path={ROUTES.SINGUP} component={SignUp} />
-					<Route exact path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
-				</Router>
+				<Switch location={background || location}>
+					<Route exact path={ROUTES.HOME} component={Home} />
+					<Route path={ROUTES.BUSCAR} component={Buscar} />
+					<Route path={ROUTES.NEW_POST} component={NewPost} />
+
+					<Route path={ROUTES.LOGIN} component={Login} />
+					<Route path={ROUTES.SINGUP} component={SignUp} />
+					<Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
+				</Switch>
+
+				{background && <Route path={ROUTES.ACCOUNT} component={Account} />}
 			</ThemeProvider>
 		</GlobalState>
 	);
-}
+};
 
 export default App;
