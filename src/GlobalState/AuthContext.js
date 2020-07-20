c3e06c70513c4fc51884cf68a8b17750b686ee6f
ferueda <fe.rueda@outlook.com>
2020-07-19 21:37:41 -0400

    update AuthContext.js added isLoading and error state and isLoading validation

--- src/GlobalState/AuthContext.js
+++ src/GlobalState/AuthContext.js
@@ -1,15 +1,21 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
+
+import useAuth from '../hooks/useAuth';
 
 const AuthContext = React.createContext(null);
 
 const AuthContextProvider = ({ children }) => {
-	const [user, setUser] = useState(null);
+	const { user, isLoading, error } = useAuth();
 
 	const memoizedContextObject = useMemo(() => {
 		return {
 			user,
+			isLoading,
+			error,
 		};
-	}, [user]);
+	}, [user, isLoading, error]);
+
+	if (isLoading) return <h1>Loading</h1>;
 
 	return <AuthContext.Provider value={memoizedContextObject}>{children}</AuthContext.Provider>;
 };
