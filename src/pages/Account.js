4b40f343d1a8618f8fd2378152df7183155d6f54
ferueda <fe.rueda@outlook.com>
2020-07-21 23:25:23 -0400

    add functionality to Account.js added the logout button and the LoginManagement component that manages the social login and email login linking

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -5,14 +5,25 @@ import { useHistory } from 'react-router-dom';
 import { FirebaseContext } from '../GlobalState/FirebaseContext';
 import useProtectedRoute from '../hooks/useProtectedRoute';
 
+import * as ROUTES from '../constants/routes';
+
 import Nav from '../components/shared/Nav';
+import LoginManagement from '../components/Auth/LoginManagement';
 
 const SideAccount = () => {
 	const firebase = useContext(FirebaseContext);
+	const history = useHistory();
+
+	const handleLogOut = () => {
+		firebase.doSignOut().then(() => {
+			history.push(ROUTES.LOGIN);
+		});
+	};
 
 	return (
 		<Box pos="fixed" top="0" right="0" bottom="0" bg="white" w="40%" minW="14rem" maxW="20rem">
-			<Button onClick={firebase.doSignOut}>Log Out</Button>
+			<Button onClick={handleLogOut}>Log Out</Button>
+			<LoginManagement />
 		</Box>
 	);
 };
