e2e359cbf280af1f4abc9cdc775003fc8a2bf82a
ferueda <fe.rueda@outlook.com>
2020-07-21 23:27:02 -0400

    improve FacebookLoginButton.js improved the login functionality of the component and refactored to async/await

--- src/components/Auth/FacebookLoginButton.js
+++ src/components/Auth/FacebookLoginButton.js
@@ -4,17 +4,63 @@ import { GrFacebookOption } from 'react-icons/gr';
 
 import { FirebaseContext } from '../../GlobalState/FirebaseContext';
 
-const FacebookLoginButton = () => {
+const errorMessages = {
+	'auth/account-exists-with-different-credential':
+		'Ya existe una cuenta registrada con este correo electrónico. Intenta ingresar con Facebook o con tu correo electrónico y en configuración podrás vincular ambas cuentas.',
+};
+
+const FacebookLoginButton = ({ isLoading = false, setIsLoading, setError }) => {
 	const firebase = useContext(FirebaseContext);
+
+	const handleSignIn = async () => {
+		setIsLoading(true);
+		setError(null);
+
+		try {
+			const { user, additionalUserInfo } = await firebase.doSignInWithFacebook();
+
+			if (additionalUserInfo.isNewUser) {
+				let username = user.displayName.toLowerCase().trim().replace(/\s/g, '');
+
+				const userisTaken = await firebase.db.collection('usernames').doc(username).get();
+
+				if (userisTaken.exists) {
+					username = `${username}${Math.floor(Math.random() * 10000)}`;
+				}
+
+				await Promise.all([
+					await user.updateProfile({
+						displayName: username,
+					}),
+					firebase.db.collection('users').doc(user.uid).set({
+						email: user.email,
+						username,
+						firstName: additionalUserInfo.profile.first_name,
+						lastName: additionalUserInfo.profile.last_name,
+					}),
+					firebase.db.collection('usernames').doc(username).set({
+						uid: user.uid,
+					}),
+				]);
+			}
+
+			setIsLoading(false);
+			setError(null);
+		} catch (error) {
+			setIsLoading(false);
+			setError({ ...error, message: errorMessages[error.code] });
+		}
+	};
 
 	return (
 		<Button
 			aria-label="ingresar con facebook"
+			isLoading={isLoading}
 			color="#fff"
 			bg="#3b5998"
 			_hover={{ bg: '#1f4287' }}
 			my="1rem"
-			onClick={firebase.doSignInWithFacebook}
+			onClick={handleSignIn}
 		>
 			<span style={{ position: 'absolute', left: '0.5rem' }}>
 				<GrFacebookOption size={24} />
