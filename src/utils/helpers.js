8c7171cc5dd87584e5d8feef074bd3de7528ffeb
ferueda <fe.rueda@outlook.com>
2020-07-22 22:24:21 -0400

    add moment.js added moment.js dep

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,10 @@
 export const capitalizeWord = word => word.trim().replace(/^\w/, c => c.toUpperCase());
+
+export const formatNumber = num => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+export const getDateDiff = (d1, d2) => {
+	const utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate());
+	const utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate());
+
+	return Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+};
