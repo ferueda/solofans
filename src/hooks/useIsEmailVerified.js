a2aecd7a15a87b133f114ce6190a5f9568a94f97
ferueda <fe.rueda@outlook.com>
2020-07-23 21:07:39 -0400

    fix typo in iseIsEmailVerified.js fixed typo in the setIsEmailverified function

--- src/hooks/useIsEmailVerified.js
+++ src/hooks/useIsEmailVerified.js
@@ -3,7 +3,7 @@ import { useEffect, useContext, useState } from 'react';
 import { AuthContext } from '../GlobalState/AuthContext';
 
 const useIsEmailVerified = (isHold = false) => {
-	const [isEmailVerified, setisEmailVerified] = useState(false);
+	const [isEmailVerified, setIsEmailVerified] = useState(false);
 	const { user, isLoading, error } = useContext(AuthContext);
 
 	useEffect(() => {
@@ -12,9 +12,9 @@ const useIsEmailVerified = (isHold = false) => {
 		if (isLoading || error || isHold) return;
 
 		if (user && !user.emailVerified && user.providerData.map(provider => provider.providerId).includes('password')) {
-			!isCancel && setisEmailVerified(false);
+			!isCancel && setIsEmailVerified(false);
 		} else {
-			!isCancel && setisEmailVerified(true);
+			!isCancel && setIsEmailVerified(true);
 		}
 
 		return () => {
