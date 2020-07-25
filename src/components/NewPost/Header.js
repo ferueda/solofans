50eb038251b70ab9a1e17a9ea1ff37882405d04e
ferueda <fe.rueda@outlook.com>
2020-07-24 20:48:38 -0400

    create NewPostHeader created the NewPostHeader component

---
+++ src/components/NewPost/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Box, Text, PseudoBox, Button } from '@chakra-ui/core';
+import { BsX } from 'react-icons/bs';
+
+const Header = ({ title, buttonLabel, buttonIsActive, isLoading = false, onSubmit }) => {
+	const history = useHistory();
+	return (
+		<Box d="flex" alignItems="center" width="100%">
+			<PseudoBox as="button" outline="none" d="flex" alignItems="center" onClick={() => history.goBack()}>
+				<BsX size={38} />
+			</PseudoBox>
+			<Text as="h2" fontSize="1.5rem" ml="1rem">
+				{title.toUpperCase()}
+			</Text>
+			<Button
+				isDisabled={!buttonIsActive}
+				isLoading={isLoading}
+				aria-label={buttonLabel}
+				variantColor="blue"
+				type="submit"
+				ml="auto"
+				onClick={onSubmit}
+			>
+				{buttonLabel}
+			</Button>
+		</Box>
+	);
+};
+
+export default Header;
