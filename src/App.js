82e6bb850be35010fa137599a76ab08f30a36513
ferueda <fe.rueda@outlook.com>
2020-07-24 20:52:13 -0400

    reestructure App.js modified how the AccountSideBar component is rendered

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
 import GlobalState from './GlobalState/GlobalState';
@@ -9,7 +9,6 @@ import * as ROUTES from './constants/routes';
 import Home from './pages/Home';
 import Buscar from './pages/Buscar';
 import NewPost from './pages/NewPost';
-import Account from './pages/Account';
 
 import Login from './components/Auth/Login';
 import SignUp from './components/Auth/SignUp';
@@ -21,16 +20,12 @@ Validate and redesign recover password template, and  flow.
 */
 
 const App = () => {
-	const location = useLocation();
-
-	const background = location.state && location.state.background;
-
 	return (
 		<GlobalState>
 			<ThemeProvider>
 				<CSSReset />
 
-				<Switch location={background || location}>
+				<Switch>
 					<Route exact path={ROUTES.HOME} component={Home} />
 					<Route path={ROUTES.BUSCAR} component={Buscar} />
 					<Route path={ROUTES.NEW_POST} component={NewPost} />
@@ -39,8 +34,6 @@ const App = () => {
 					<Route path={ROUTES.SINGUP} component={SignUp} />
 					<Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
 				</Switch>
-
-				{background && <Route path={ROUTES.ACCOUNT} component={Account} />}
 			</ThemeProvider>
 		</GlobalState>
 	);
