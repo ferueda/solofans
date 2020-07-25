296ed090ae5ff34dee49f620f6dc39a6d7bae238
ferueda <fe.rueda@outlook.com>
2020-07-24 20:53:13 -0400

    refactor Nav.js refactored the Nav component and how it is rendered. Removed the useHistory based rendering

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -1,16 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as RouterLink, useHistory } from 'react-router-dom';
 import { Box, Link } from '@chakra-ui/core';
 import { BsPlusSquare, BsHouse, BsSearch, BsChatDots, BsPeopleCircle } from 'react-icons/bs';
 
 import * as ROUTES from '../../constants/routes';
 
-const breakpoints = ['30em', '48em', '62em', '80em'];
+import useLocationBlocker from '../../hooks/useLocationBlocker';
 
-breakpoints.sm = breakpoints[0];
-breakpoints.md = breakpoints[1];
-breakpoints.lg = breakpoints[2];
-breakpoints.xl = breakpoints[3];
+import AccountSidebar from '../Account/AccountSidebar';
 
 const statusColor = {
 	active: '#3182CE',
@@ -18,61 +15,73 @@ const statusColor = {
 };
 
 const Nav = () => {
+	const [isModal, setIsModal] = useState(false);
+
 	const { location } = useHistory();
 
+	useLocationBlocker();
+
 	return (
-		<Box
-			as="nav"
-			pos="fixed"
-			width="100%"
-			bottom="0"
-			left="0"
-			d="flex"
-			alignContent="center"
-			py="1rem"
-			px="1.5rem"
-			borderTop="1px"
-			borderColor="gray.300"
-			bg="white"
-			zIndex={2}
-		>
-			<Box d="flex" alignItems="center" justifyContent="space-between" w="100%" h="100%">
-				<Link as={RouterLink} to={ROUTES.HOME}>
-					<BsHouse size={28} color={location.pathname === ROUTES.HOME ? statusColor.active : statusColor.notActive} />
-				</Link>
+		<React.Fragment>
+			<Box
+				as="nav"
+				pos="fixed"
+				width="100%"
+				bottom="0"
+				left="0"
+				d="flex"
+				alignContent="center"
+				py="1rem"
+				px="1.5rem"
+				borderTop="1px"
+				borderColor="gray.300"
+				bg="white"
+				zIndex={2}
+			>
+				<Box d="flex" alignItems="center" justifyContent="space-between" w="100%" h="100%">
+					<Link _focus="none" as={RouterLink} to={ROUTES.HOME}>
+						<BsHouse size={28} color={location.pathname === ROUTES.HOME ? statusColor.active : statusColor.notActive} />
+					</Link>
 
-				<Link as={RouterLink} to={ROUTES.BUSCAR}>
-					<BsSearch
-						size={28}
-						color={location.pathname === ROUTES.BUSCAR ? statusColor.active : statusColor.notActive}
-					/>
-				</Link>
+					<Link _focus="none" as={RouterLink} to={ROUTES.BUSCAR}>
+						<BsSearch
+							size={28}
+							color={location.pathname === ROUTES.BUSCAR ? statusColor.active : statusColor.notActive}
+						/>
+					</Link>
 
-				<Link as={RouterLink} to={ROUTES.NEW_POST}>
-					<BsPlusSquare
-						size={28}
-						color={location.pathname === ROUTES.NEW_POST ? statusColor.active : statusColor.notActive}
-					/>
-				</Link>
+					<Link _focus="none" as={RouterLink} to={ROUTES.NEW_POST}>
+						<BsPlusSquare
+							size={28}
+							color={location.pathname === ROUTES.NEW_POST ? statusColor.active : statusColor.notActive}
+						/>
+					</Link>
 
-				<Link as={RouterLink} to="#">
-					<BsChatDots size={28} color={statusColor.notActive} />
-				</Link>
+					<Link _focus="none" as={RouterLink} to="#">
+						<BsChatDots size={28} color={statusColor.notActive} />
+					</Link>
 
-				<Link
-					as={RouterLink}
-					to={{
-						pathname: location.pathname === ROUTES.ACCOUNT ? location.state.background.pathname : ROUTES.ACCOUNT,
-						state: location.pathname === ROUTES.ACCOUNT ? null : { background: location },
-					}}
-				>
-					<BsPeopleCircle
-						size={28}
-						color={location.pathname === ROUTES.ACCOUNT ? statusColor.active : statusColor.notActive}
-					/>
-				</Link>
+					<Box as="button" outline="none" onClick={() => setIsModal(state => !state)}>
+						<BsPeopleCircle size={28} color={isModal ? statusColor.active : statusColor.notActive} />
+					</Box>
+				</Box>
 			</Box>
-		</Box>
+
+			{isModal && (
+				<React.Fragment>
+					<Box
+						pos="fixed"
+						top="0"
+						right="0"
+						bottom="0"
+						left="0"
+						backgroundColor="rgba(0, 0, 0, 0.15)"
+						onClick={() => setIsModal(false)}
+					></Box>
+					<AccountSidebar />
+				</React.Fragment>
+			)}
+		</React.Fragment>
 	);
 };
 
