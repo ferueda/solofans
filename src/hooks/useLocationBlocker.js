ca8124b59cd85a76a39bc2c472e6e2fd86268954
ferueda <fe.rueda@outlook.com>
2020-07-24 21:31:08 -0400

    create useLocationBlocker.js created custom hook that blocks saving in react router history when visiting a Link twice or more

---
+++ src/hooks/useLocationBlocker.js
@@ -0,0 +1,19 @@
+import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const useLocationBlocker = () => {
+	const history = useHistory();
+	useEffect(
+		() =>
+			history.block(
+				(location, action) => action !== 'PUSH' || getLocationId(location) !== getLocationId(history.location)
+			),
+		[] // eslint-disable-line react-hooks/exhaustive-deps
+	);
+};
+
+const getLocationId = ({ pathname, search, hash }) => {
+	return pathname + (search ? '?' + search : '') + (hash ? '#' + hash : '');
+};
+
+export default useLocationBlocker;
