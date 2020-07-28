47a2948bf77f2f8257e3021f7a6a6907aafad118
ferueda <fe.rueda@outlook.com>
2020-07-27 21:39:42 -0400

    fix PostBody.js fixed bug that long text where overflowing outside the container

--- src/components/Post/PostBody.js
+++ src/components/Post/PostBody.js
@@ -4,10 +4,10 @@ import { Box, Image, Text } from '@chakra-ui/core';
 const PostBody = ({ src, username, date, caption }) => {
 	return (
 		<Box d="flex" flexDir="column" alignItems="stretch" justifyItems="stretch" maxW="600px">
-			<Box px="1rem" pb="1rem" width="100%">
-				<Text>{caption}</Text>
+			<Box d="flex" flexWrap="wrap" px="1rem" pb="1rem" width="100%">
+				<Text wordBreak="break-word">{caption}</Text>
 			</Box>
-			<Image src={src} alt={`Foto de ${username} subida el ${date}`}></Image>
+			<Image src={src} alt={`foto de ${username} subida el ${date}`}></Image>
 		</Box>
 	);
 };
