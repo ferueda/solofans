e649d8732b5b06fd55331a08635809d7c82b0b99
ferueda <fe.rueda@outlook.com>
2020-07-29 23:16:39 -0400

    create ImagePreview.js created the component that renders the preview of the images being uploaded to firebase storage

---
+++ src/components/NewPost/ImagePreview.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, PseudoBox, Image } from '@chakra-ui/core';
+import { BsX } from 'react-icons/bs';
+
+const ImagePreview = ({ image, preview, dispatch, imageInputRef }) => {
+	const handleRemoveImage = () => {
+		dispatch({ type: 'REMOVE_IMAGE', payload: { image: image.name } });
+		imageInputRef.current.value = '';
+	};
+
+	return (
+		<Box pos="relative">
+			<Image borderRadius="0.25rem" w="130px" h="130px" src={preview} alt="image preview" />
+			<PseudoBox
+				as="button"
+				outline="none"
+				d="flex"
+				alignItems="center"
+				onClick={handleRemoveImage}
+				position="absolute"
+				top="5px"
+				right="5px"
+				bg="blue.500"
+				borderRadius="9999px"
+				_hover={{ bg: 'blue.600' }}
+			>
+				<BsX size={20} color="white" />
+			</PseudoBox>
+		</Box>
+	);
+};
+
+export default ImagePreview;
