3982027f1e89a86fc654bae1f8e3f12198563f15
ferueda <fe.rueda@outlook.com>
2020-07-31 20:30:13 -0400

    add image format validation to NewPost.js

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,5 +1,5 @@
 import React, { useReducer, useContext, useRef } from 'react';
-import { Box, Divider, Text } from '@chakra-ui/core';
+import { Box, Divider, Text, Grid } from '@chakra-ui/core';
 
 import { AuthContext } from '../GlobalState/AuthContext';
 
@@ -26,6 +26,25 @@ const initialState = {
 	isModal: false,
 	error: null,
 };
+
+/*
+		TODO:
+			VALIDATE IMAGES FORMAT ON THE SERVER
+			WHEN POSTING WENT WRONG, REVERT EVERYTHING THAT WAS DONE TILL THAT POINT, DELETE IMAGES FROM SERVER, DELETE DE POST, ETC
+			RESEARCH HOW TO CANCEL FIREBASE REQUESTS
+			CHANGE FEED POSTS REQUESTS TO GET (NOT CONTINIUOUS UPDATE)
+			CREATE A BG FUNCTION THAT DELETES A POST IMAGES WHEN THE POST IS DELETED. ALSO REMOVE IT FROM RECENT POSTS IF IT'S THERE
+			VALIDATE IMAGES SIZE IN THE FRONT
+			CREATE A CLOUD FUNCTION THAT RESIZES THE IMAGE WHEN UPLOADED
+			CREATE A CLOUD FUNCTION THAT RESIZES THE AVATAR IMAGE WHEN UPLOADED SO IT DOESN'T USE THAT MUCH SPACE. REFER TO THE FIRESHIP CLOUD 		FUNCTION RESIZE VIDEO
+			FIX THE POST IMAGE SIZES SO IT ONLY RESTRICTS THE WIDTH AND NOT THE HEIGHT. 
+			PASS THE FUNCTIONALITY THAT CREATES A RECENT POSTS IN FOLLOWERS TO A CLOUD FUNCTION INSTEAD OF DOING IT ON THE CLIENT
+			ADD A SECURITY RULE FOR THE PRICE OF THE POST, WHERE IT CAN'T BE LOWER THAN $1.00 OR HIGHER THAN $100.000.
+			CHANGES IN USER PROFILE PIC OR USERNAME SHOULD BE PROPAGATED USING CLOUD FUNCTIONS AND NOT THE CLIENT.
+			ALSO USE FUNCTIONS TO MAKE CRON JOBS AND MAINTANCE JOBS.
+			CREATE A LOADING BLOCKED IMAGE WHEN LOADING STATE SO THE POST DESCRP AND IMAGES CAN'T BE CHANGED WHILE LOADING.
+
+ */
 
 const NewPost = () => {
 	useProtectedRoute();
@@ -65,6 +84,10 @@ const NewPost = () => {
 
 	const handleAddImage = ({ target }) => {
 		const image = target.files[0];
+
+		if (image && image.type !== 'image/jpeg' && image.type !== 'image/png') {
+			return dispatch({ type: 'FORMAT_ERROR', payload: { error: { message: 'El archivo debe ser una imagen' } } });
+		}
 
 		if (image && images.some(img => img.image.name === image.name)) {
 			return dispatch({ type: 'FORMAT_ERROR', payload: { error: { message: 'La imagen ya fue seleccionada' } } });
@@ -108,6 +131,7 @@ const NewPost = () => {
 				dispatch={dispatch}
 				captionShouldLength={captionShouldLength}
 				imageInputRef={imageInputRef}
+				isLoading={isLoading}
 			/>
 			<Divider borderColor="gray.200" width="100%" my="0.5rem" />
 
@@ -116,15 +140,17 @@ const NewPost = () => {
 			<Meta isLocked={isLocked} price={price} />
 			<Divider borderColor="gray.200" width="100%" my="0.5rem" />
 
-			{images.map(({ image, preview }) => (
-				<ImagePreview
-					key={image.name}
-					imageInputRef={imageInputRef}
-					image={image}
-					preview={preview}
-					dispatch={dispatch}
-				/>
-			))}
+			<Grid gap="0.5rem" width="100%" justifyContent="center" templateColumns="repeat(auto-fit, 120px)" mt="1rem">
+				{images.map(({ image, preview }) => (
+					<ImagePreview
+						key={image.name}
+						imageInputRef={imageInputRef}
+						image={image}
+						preview={preview}
+						dispatch={dispatch}
+					/>
+				))}
+			</Grid>
 
 			<PriceModal isOpen={isModal} isLocked={isLocked} dispatch={dispatch} />
 			<Nav />
